c282217b46943c33e75643d28ba5e162d3b01aee
souzarica <souza.rica@gmail.com>
2016-10-05 08:26:59 -0300

    Animação tela Login
    Ricardo

---
+++ studioactions/mobile/AnimationLoginPreShowMoveWidgets.js
@@ -0,0 +1,13 @@
+function AnimationLoginPreShowMoveWidgets(eventobject) {
+    return AS_Form_761ca8ac1b5f4c328b72d20ac753b786(eventobject);
+}
+
+function AS_Form_761ca8ac1b5f4c328b72d20ac753b786(eventobject) {
+    frmLogin.imgLogo.top = "130%";
+    frmLogin.imgLogo.width = "10%";
+    frmLogin.flxLogin.centerX = "-50%";
+    frmLogin.imgBola.right = "120%";
+    frmLogin.imgBolaBask.top = "-50%";
+    frmLogin.imgBolaTennis.left = "-70%";
+    frmLogin.imgBolaTennis.bottom = "60%";
+}
